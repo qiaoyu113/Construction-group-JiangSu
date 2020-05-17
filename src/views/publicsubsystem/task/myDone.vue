43830c1749fcb200d25dfd49710adea36d3461fd
wangtao <wangtao14385@gmail.com>
2020-05-17 21:48:00 +0800

    myDone.vue

--- src/views/publicsubsystem/task/myDone.vue
+++ src/views/publicsubsystem/task/myDone.vue
@@ -1,39 +1,49 @@
 <template>
 <div class="mod-role">
-  <t-form  @submit.native.prevent @keyup.enter.native="doRefresh()" label-width="120px">
-    <el-row :gutter="20">
-      <el-col :span="8">
-        <el-form-item label="流程类别">
-          <el-select placeholder="请选择流程类别" v-model="gridOptions.dataSource.serviceInstanceInputParameters.processDefinationKey" clearable>
-            <el-option v-for="(item, index) in processDefinationlist" :key='item.key' :label="item.name" :value="item.key"></el-option>
-          </el-select>
-        </el-form-item>
-      </el-col>
-      <el-col :span="14">
-        <el-form-item label="流程发起时间">
-          <t-datetime-range-picker v-model="startDateRange" @change="onStartDateRangeChanged">
-          </t-datetime-range-picker>
-        </el-form-item>
-      </el-col>
-    </el-row>
-    <el-row :gutter="20">
-      <el-col :span="8">
-        <el-form-item label="关键字">
-          <el-input  @submit.native.prevent @keyup.enter.native="doRefresh()" v-model="gridOptions.dataSource.serviceInstanceInputParameters.searchKey" placeholder="单据描述" clearable></el-input>
-        </el-form-item>
-      </el-col>
-      <el-col :span="14">
-        <el-form-item>
-          <el-button  @click="doRefresh()" icon="el-icon-search">查询</el-button>
-          <el-button  icon="el-icon-download" @click="doExportExcel()">
-            <i class="fa fa-lg fa-level-down"></i>导出
-          </el-button>
-        </el-form-item>
-      </el-col>
-    </el-row>
-  </t-form>
-  <t-grid ref="searchReulstList" :options="gridOptions" @selection-change="handleSelectionChange">
-  </t-grid>
+  <el-card shadow="never">
+    <t-form  @submit.native.prevent @keyup.enter.native="doRefresh()" label-width="120px" :model="gridOptions.dataSource.serviceInstanceInputParameters">
+      <el-row :gutter="10" class="search-top-operate">
+        <el-button type="primary" plain icon="el-icon-download" @click="doExportExcel()">导出</el-button>
+      </el-row>
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <el-form-item label="流程名称" prop="searchKey">
+            <t-input @submit.native.prevent @keyup.enter.native="doRefresh()" v-model="gridOptions.dataSource.serviceInstanceInputParameters.searchKey" clearable></t-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="项目名称" prop="searchKey1">
+            <t-input @submit.native.prevent @keyup.enter.native="doRefresh()" v-model="gridOptions.dataSource.serviceInstanceInputParameters.searchKey1" clearable></t-input>
+          </el-form-item>
+        </el-col>
+        <!-- <el-col :span="8">
+          <el-form-item label="流程发起时间">
+            <t-datetime-range-picker v-model="startDateRange" @change="onStartDateRangeChanged">
+            </t-datetime-range-picker>
+          </el-form-item>
+        </el-col> -->
+        <el-col :span="8">
+          <el-form-item label="流程类别" prop="processDefinationKey">
+            <el-select placeholder="请选择流程类别" v-model="gridOptions.dataSource.serviceInstanceInputParameters.processDefinationKey" clearable>
+              <el-option v-for="(item, index) in processDefinationlist" :key='item.key' :label="item.name" :value="item.key"></el-option>
+            </el-select>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row type="flex" :span="8" justify="end" class="search-bottom-operate">
+        <el-col :span="12">
+          <el-form-item>
+            <el-button  @click="doRefresh()" type="primary" icon="el-icon-search">查询</el-button>
+            <el-button  @click="doReset()">
+              <i class="el-icon-delete"></i>清空
+            </el-button>
+          </el-form-item>
+        </el-col>
+      </el-row>
+    </t-form>
+    <t-grid ref="searchReulstList" :options="gridOptions" @selection-change="handleSelectionChange">
+    </t-grid>
+  </el-card>
 </div>
 </template>
 <script>
@@ -57,17 +67,17 @@ export default {
         grid: {   
           offsetHeight: 125, //125:查询部分高度
           mutiSelect: false,
-          operates: {
-            width: 60,
-            fixed: 'left',
-            list: [{
-              type: 'text',
-              show: true,
-              label: '详情',
-              render: this.renderTaskBtnLabel,
-              method: this.doView,
-            }, ]
-          }, // 列操作按钮
+          // operates: {
+          //   width: 60,
+          //   fixed: 'left',
+          //   list: [{
+          //     type: 'text',
+          //     show: true,
+          //     label: '详情',
+          //     render: this.renderTaskBtnLabel,
+          //     method: this.doView,
+          //   }, ]
+          // }, // 列操作按钮
           columns: [{
              fixed: 'left',
               prop: 'processDefinationName',
@@ -84,7 +94,7 @@ export default {
             },
             {
               prop: 'startDate',
-              label: '提单日期',
+              label: '项目名称',
               sortable: true,
               width: 110,
               formatter: (row, column, cellValue) => {
@@ -93,7 +103,7 @@ export default {
             },
               {
               prop: 'endDate',
-              label: '结束日期',
+              label: '发起部门',
               sortable: true,
               width: 110,
               formatter: (row, column, cellValue) => {
@@ -102,14 +112,32 @@ export default {
             },
             {
               prop: 'folio',
-              label: '单据描述',
+              label: '发起人',
+              sortable: true,
+              minWidth: 120,
+            },
+            {
+              prop: 'folio',
+              label: '发起时间',
+              sortable: true,
+              minWidth: 120,
+            },
+            {
+              prop: 'folio',
+              label: '当前节点',
+              sortable: true,
+              minWidth: 120,
+            },
+            {
+              prop: 'folio',
+              label: '当前办理人',
               sortable: true,
               minWidth: 120,
             },
             {
               prop: 'status',
               columnKey: 'status',
-              label: '状态',
+              label: '流程状态',
               sortable: true,
               width: 90,
               filters: [{
@@ -140,30 +168,30 @@ export default {
                 }
               },
             },
-            {
-              prop: 'taskInfo',
-              label: '任务信息',
-              sortable: false,
-              minWidth: 250,
-              render: (h, params) => {
-                let row = params.row;
-                if (!row.tasklist) {
-                  return h('');
-                }
-                let wfTasks = row.tasklist;
-                wfTasks.forEach(p=>{
-                  p.processInstStatus = row.status;
-                  p.processDefinationKey = row.processDefinationKey;
-                })
+            // {
+            //   prop: 'taskInfo',
+            //   label: '任务信息',
+            //   sortable: false,
+            //   minWidth: 250,
+            //   render: (h, params) => {
+            //     let row = params.row;
+            //     if (!row.tasklist) {
+            //       return h('');
+            //     }
+            //     let wfTasks = row.tasklist;
+            //     wfTasks.forEach(p=>{
+            //       p.processInstStatus = row.status;
+            //       p.processDefinationKey = row.processDefinationKey;
+            //     })
 
-                return h('wf-taskinfo', {
-                  props: {
-                    tasklist: wfTasks,
-                    disabled:true,
-                  }, // 组件的props
-                })
-              }
-            },
+            //     return h('wf-taskinfo', {
+            //       props: {
+            //         tasklist: wfTasks,
+            //         disabled:true,
+            //       }, // 组件的props
+            //     })
+            //   }
+            // },
           ], // 需要展示的列
           defaultSort: {
             prop: 'id',
@@ -199,7 +227,10 @@ export default {
     handleSelectionChange(val) {
       this.checkededRows = val;
     },
-
+    doReset() {
+      this.$refs.search.resetFields();
+      this.doRefresh();
+    },
     doExportExcel() {
       this.$refs.searchReulstList.exportCSV('已办列表');
     },
