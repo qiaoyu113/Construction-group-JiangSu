67862e1e4129f83d66b5cc9508489732c359d1fa
mingyujiao <wsxqaz159357>
2020-05-22 01:46:33 +0800

    1.这是一个bug页面

---
+++ src/components/popup/bank-select.vue
@@ -0,0 +1,210 @@
+<template>
+  <div class="project-select">
+    <t-input v-model="currentValue" :placeholder="placeholder" :disabled="true" :readOnly="readOnly">
+      <i slot="suffix" class="el-input__icon el-icon-search" @click="dialogFormVisible = true"></i>
+    </t-input>
+    <el-dialog title="银行选择" :visible.sync="dialogFormVisible" width='80%' center @close="doReset()">
+      <t-form ref="search" @submit.native.prevent @keyup.enter.native="doRefresh()" label-width="120px" :model="gridOptions.dataSource.serviceInstanceInputParameters">
+        <el-row :gutter="10" class="search-top-operate">
+          <el-button type="primary" @click="proChoose()">
+            确定
+          </el-button>
+          <el-button type="info" @click="dialogFormVisible = false">
+            取消
+          </el-button>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="8">
+            <el-form-item label="项目名称" prop="proName" style="margin-bottom: 15px;">
+              <t-input @submit.native.prevent @keyup.enter.native="doRefresh()" v-model="gridOptions.dataSource.serviceInstanceInputParameters.proName"
+                       placeholder="项目名称" clearable></t-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="建设单位" prop="proConstructCompany" style="margin-bottom: 15px;">
+              <t-input @submit.native.prevent @keyup.enter.native="doRefresh()" v-model="gridOptions.dataSource.serviceInstanceInputParameters.proConstructCompany"
+                       placeholder="建设单位" clearable></t-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="工程类别" prop="proType" style="margin-bottom: 15px;">
+              <t-dic-dropdown-select dicType="engineering_type" v-model="gridOptions.dataSource.serviceInstanceInputParameters.proType"></t-dic-dropdown-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="经营方式" prop="proRunMode" style="margin-bottom: 15px;">
+              <t-dic-dropdown-select dicType="business_type" v-model="gridOptions.dataSource.serviceInstanceInputParameters.proRunMode"></t-dic-dropdown-select>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row type="flex" :span="8" justify="space-between" class="search-bottom-operate">
+          <el-col :span="8">
+            <el-form-item label="选择的项目" prop="selectedProname" style="margin-bottom: 15px;">
+              <t-input v-model="selectProject.proName" :readOnly="true" placeholder="还未选择项目"></t-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item style="margin-bottom: 15px;">
+              <el-button @click="doRefresh()" type="primary" icon="el-icon-search">查询</el-button>
+              <el-button @click="doReset()" icon="el-icon-circle-close">清空</el-button>
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </t-form>
+      <t-grid ref="searchReulstList" :options="gridOptions" @cell-click="handleCellClick"></t-grid>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+  import util from '@/util'
+  import moment from 'moment'
+  import baseView from '@/base/baseView'
+
+  export default {
+    extends: baseView,
+    props: {
+      value: {
+        type: String,
+        default: '',
+        required: false
+      },
+      placeholder: {
+        type: String,
+        default: '选择一个银行账号',
+        required: false
+      },
+      readOnly: {
+        type: Boolean,
+        default: false,
+        required: false,
+      }
+    },
+    data() {
+      return {
+        dataForm: {
+
+        },
+        dataRules: {
+          proName:  [
+            {required: false}
+          ],
+          proType:  [
+            {required: false}
+          ],
+          proRunMode:  [
+            {required: false}
+          ],
+          proConstructCompany:  [
+            {required: false}
+          ],
+          selectedProname:  [
+            {required: false}
+          ],
+        },
+        selectProject: {
+          proCode: ''
+        },
+        currentValue: '',
+        gridOptions: {
+          dataSource: {
+            serviceInstance: tapp.services.finaFwaccounapproval.getPagedList,
+            serviceInstanceInputParameters: {
+              searchKey: null,
+            }
+          },
+          grid: {
+            offsetHeight: 125, //125:查询部分高度
+            mutiSelect: false,
+            columns: [
+              {
+                prop: 'proName',
+                label: '项目名称',
+                sortable: true
+              },
+              {
+                prop: 'proCode',
+                label: '项目编码',
+                sortable: true
+              },
+              {
+                prop: 'bankName',
+                label: '开户行名称',
+                sortable: true
+              },
+              {
+                prop: 'bankAddress',
+                label: '开户网点',
+                sortable: true
+              },
+              {
+                prop: 'bankAccountName',
+                label: '银行帐户名称',
+                sortable: true
+              },
+              {
+                prop: 'bankAccount',
+                label: '银行帐号',
+                sortable: true
+              },
+              {
+                prop: 'openTime',
+                label: '开户时间',
+                sortable: true,
+                formatter: (row, column, cellValue) => {
+                  return this.$util.dateFormat(row.openTime, 'YYYY-MM-DD');
+                }
+              }
+            ], // 需要展示的列
+            defaultSort: {
+              prop: 'id',
+              order: 'descending'
+            },
+          }
+        },
+        dialogFormVisible: false,
+      }
+    },
+    components: {},
+    created() {
+      this.currentValue = this.value
+    },
+    watch: {
+      currentValue(newValue, oldValue) {
+
+      }
+    },
+    activated() {
+      this.$nextTick(() => {})
+    },
+    computed: {},
+    methods: {
+      clearValidate() {
+        this.$nextTick((_) => {
+          this.$refs.ruleForm.clearValidate()
+        })
+      },
+      doRefresh() {
+        this.$refs.searchReulstList.refresh();
+      },
+      doReset() {
+        this.$refs.search.resetFields()
+        this.selectProject = { proName: '' }
+        this.gridOptions.dataSource.serviceInstanceInputParameters = {}
+        this.doRefresh();
+      },
+      handleCellClick(row, value, cell, event) {
+        this.selectProject = row;
+      },
+      proChoose() {
+        //传送到父组件
+        this.currentValue = this.selectProject.proName;
+        this.$emit('selectedProject', this.selectProject);
+        this.$emit('input', this.selectProject.id);
+        this.doReset();
+        this.dialogFormVisible = false
+      }
+    }
+  }
+</script>
+
