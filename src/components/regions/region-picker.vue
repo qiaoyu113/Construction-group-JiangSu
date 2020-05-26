bf52b304aec6da502efa1a667409bde634f4cac7
wangtao <wangtao14385@gmail.com>
2020-05-26 10:03:43 +0800

    update region-picker.vue

--- src/components/regions/region-picker.vue
+++ src/components/regions/region-picker.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="region-picker">
-    <el-col :span="12" style="padding-left: 0;padding-right: 2px;">
+    <el-col :span="8" style="padding-left: 0;padding-right: 2px;">
       <el-form-item prop="province" :rules="dataRules.province">
-        <t-dic-dropdown-select dicType="base_region" v-model="province" :readOnly="readOnly" :disabled="disabled"></t-dic-dropdown-select>
+        <t-dic-dropdown-select ref="province" dicType="base_region" v-model="_province" :readOnly="readOnly" :disabled="disabled" @input="provinceChange"></t-dic-dropdown-select>
       </el-form-item>
     </el-col>
-    <el-col :span="12" style="padding-left: 0;padding-right: 0;">
+    <el-col :span="8" style="padding-left: 0;padding-right: 0;">
       <el-form-item prop="city" :rules="dataRules.city">
-        <t-dic-dropdown-select :dataisgood="currentProvince.items" v-model="city" :readOnly="readOnly" :disabled="disabled"></t-dic-dropdown-select>
+        <t-dic-dropdown-select ref="cityPicker" :dataisgood="currentProvince.items" v-model="_city" :readOnly="readOnly" :disabled="disabled" @input="cityChange"></t-dic-dropdown-select>
+      </el-form-item>
+    </el-col>
+    <el-col :span="8" style="padding-left: 0;padding-right: 0;">
+      <el-form-item prop="city" :rules="dataRules.district">
+        <t-dic-dropdown-select ref="districtPicker" :dataisgood="currentCity.items" v-model="_district" :readOnly="readOnly" :disabled="disabled" @input="districtChange"></t-dic-dropdown-select>
       </el-form-item>
     </el-col>
   </div>
@@ -18,6 +23,21 @@ export default {
   components: {},
   props: {
     value: {
+      type: String,
+      default: '',
+      required: false
+    },
+    province: {
+      type: String,
+      default: '',
+      required: false
+    },
+    city: {
+      type: String,
+      default: '',
+      required: false
+    },
+    district: {
       type: String,
       default: '',
       required: false
@@ -40,34 +60,31 @@ export default {
   },
   data () {
     return {
-      province: '',
-      city: '',
-      currentProvince: {},
-      selectValue: '',
+      _province: '',
+      _city: '',
+      _district: '',
+      currentProvince: {
+        items: []
+      },
+      currentCity: {
+        items: []
+      },
       dataRules: {
         province: [{validator: this.isProvinceEmpty, trigger: 'change'}],
-        city: [{validator: this.isCityEmpty, trigger: 'change'}]
+        city: [{validator: this.isCityEmpty, trigger: 'change'}],
+        district: [{validator: this.isCityEmpty, trigger: 'change'}]
       }
     }
   },
-  watch: {
-    province (value) {
-      this.city = ''
-      if (!value) {
-        this.currentProvince.items = []
-        return
-      }
-      this.currentProvince = find(tapp.data.base_datadictionary['base_region'], {id: value})
-      this.$emit('province', value)
-    },
-    city (value) {
-      this.$emit('city', value)
-    }
+  created() {
+    this._province = this.province;
+    this._city = this.city;
+    this._district = this.district;
   },
   methods: {
     isProvinceEmpty (rule, value, cb) {
       if (!this.required) return cb()
-      if (this.province == null || this.province.length == 0 || this.province == '') {
+      if (this._province == null || this._province.length == 0 || this._province == '') {
         return cb(new Error('省份不能为空'))
       }
       return cb()
@@ -77,10 +94,49 @@ export default {
       if (this.currentProvince && this.currentProvince.items && this.currentProvince.items.length == 0) {
         return cb()
       }
-      if (this.city == null || this.city.length == 0 || this.city == '') {
+      if (this._city == null || this._city.length == 0 || this._city == '') {
         return cb(new Error('城市不能为空'))
       }
       return cb()
+    },
+    isDistrictEmpty (rule, value, cb) {
+      if (!this.required) return cb()
+      if (this.currentCity && this.currentCity.items && this.currentCity.items.length == 0) {
+        return cb()
+      }
+      if (this._district == null || this._district.length == 0 || this._district == '') {
+        return cb(new Error('地区不能为空'))
+      }
+      return cb()
+    },
+    provinceChange(val) {
+      this.$emit('update:city', '')
+      if(this.$refs['cityPicker']) {
+        this.$refs['cityPicker'].currentValue = '';
+      }
+      if (!val) {
+        this.currentProvince.items = []
+        return
+      }
+      this.currentProvince = find(tapp.data.base_datadictionary['base_region'], {id: val})
+      this._province = val;
+      this.$emit('update:province', val)
+    },
+    cityChange(val) {
+      this.$emit('update:district', '')
+      if(this.$refs['districtPicker']) {
+        this.$refs['districtPicker'].currentValue = '';
+      }
+      if (!val) {
+        this.currentCity.items = []
+        return
+      }
+      this.currentCity = find(this.currentProvince.items, {id: val})
+      this._city = val;
+      this.$emit('update:city', val)
+    },
+    districtChange(val) {
+      this.$emit('update:district', val)
     }
   }
 }
