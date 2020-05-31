07c64c0352bf6b0d9dedc6f0292729c530585f74
wangtao <wangtao14385@gmail.com>
2020-05-31 18:24:09 +0800

    投标准备文件审批 流程

--- src/views/tbglsubsystem/TBidFileApproval/add.vue
+++ src/views/tbglsubsystem/TBidFileApproval/add.vue
@@ -254,6 +254,7 @@
             if (this.dataForm.id) {
               let self = this
               tapp.services.tBidFileApproval.get(id).then(function (result) {
+                console.log('result', result)
                 self.dataForm = self.$util.deepObjectAssign({}, self.dataForm, result)
                 let params = {
                   filters: {},
@@ -263,9 +264,10 @@
                   id: result.pcId
                 }
                 tapp.services.tBidProcaseApproval.getPagedList(params).then(_result => {
+                  console.log('_result', _result)
                   if(_result && _result.items && _result.items.length > 0) {
                     let item = find(_result.items, {id: result.pcId})
-                    self.dataForm = self.$util.deepObjectAssign({}, self.dataForm, item)
+                    if(item) self.dataForm = self.$util.deepObjectAssign({}, self.dataForm, item)
                   }
                 })
               })
